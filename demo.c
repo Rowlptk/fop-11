44d3c1aaec599e1d6e2c3fd3208e6c80577b6aa1
Rahul Pathak <rahulzha93@gmail.com>
2020-01-13 09:31:06 +0545

    initials

---
+++ demo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    int X,Y,SUM;
+    X = 23;
+    Y = 56;
+
+    SUM = X + Y;
+
+    printf("The Sum is %d",SUM);
+}
---
+++ demopy.py
@@ -0,0 +1,6 @@
+X = 56
+Y = 34
+
+sum = X+Y
+
+print(sum)
